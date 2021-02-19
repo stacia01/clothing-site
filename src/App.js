4de9dfd206cdec2ea5f4236435842bf681430faa
unknown <staciarimta@gmail.com>
2021-02-19 13:39:15 +0700

    add collection item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Route, Link } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component'
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={ HomePage } />
+        <Route path='/shop' component={ ShopPage } />
+      </Switch>
     </div>
   );
 }
